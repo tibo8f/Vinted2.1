c6d943db1175fdcb684385140eaacfe2fc905091
tibo8f <83464121+tibo8f@users.noreply.github.com>
2024-05-23 23:19:43 +0200

    First communication with the api
    get request

--- Vinted2/app/(tabs)/create.tsx
+++ Vinted2/app/(tabs)/create.tsx
@@ -1,12 +1,54 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 
-const create = () => {
+// type Item = {
+//   id: string;
+//   title: string;
+//   releaseYear: string;
+// };
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  // const [data, setData] = useState<Item[]>([]);
+
+  const getItem = async () => {
+    try {
+      console.log("search");
+      const response = await fetch("http://192.168.1.8:3000/api/newproduct", {
+        method: "GET",
+      });
+      console.log("Get was made");
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getItem();
+  }, []);
+
   return (
-    <View>
-      <Text>create</Text>
-    </View>
+    // <View style={{ flex: 1, padding: 24 }}>
+    //   {isLoading ? (
+    //     <ActivityIndicator />
+    //   ) : (
+    //     <FlatList
+    //       data={data}
+    //       keyExtractor={({ id }) => id}
+    //       renderItem={({ item }) => (
+    //         <Text>
+    //           {item.title}, {item.releaseYear}
+    //         </Text>
+    //       )}
+    //     />
+    //   )}
+    // </View>
+    <Text>Hello</Text>
   );
 };
 
-export default create;
+export default App;
